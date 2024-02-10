9f285b5ab0ff1a101c40f1f8e6f0c74ab3e9ec23
emaenriquez <emaenriquez1985@gmail.com>
2024-02-10 19:35:02 -0300

    primer commit de aprendiendo react js

---
+++ src/Componente.jsx
@@ -0,0 +1,29 @@
+
+
+import PropTypes from 'prop-types'
+
+import './Componente.css'
+
+// const strin = 'hola mundo'
+// const number = 10
+// const skill = ['java', 'astro', 'tailwid',]
+// const funci = () => 1 + 1
+
+export const Componente = ({titulo,subTitulo}) => {
+    return (
+        <>
+            <h1>{titulo}</h1>
+            <h2>{subTitulo}</h2>
+        </>
+    )
+}
+
+Componente.PropTypes = {
+    titulo: PropTypes.string.isRequired,
+    subTitulo: PropTypes.string.isRequired
+}
+
+Componente.defaultProps = {
+    titulo: 'Bienvenidos a nuestra plataforma web',
+    subTitulo: 'donde aprenderas todo lo necesario'
+}
